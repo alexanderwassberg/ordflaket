c1d13aa76c557db5faf9196f2f694f956cae7848
alexander.wassberg <alexanderwassberg@gmail.com>
2021-12-21 13:36:37 +0100

    Fixes Layout

--- script/wordGen.js
+++ script/wordGen.js
@@ -1,24 +1,26 @@
+getWord();
+
 function getWord() {
-    fetch('https://raw.githubusercontent.com/alexanderwassberg/ordflaket2.0/main/words.txt')
-    .then(response => response.text())
-    .then(data => {
-      var wordList = data.split("\n");
-      var word = wordList[Math.floor(Math.random() * wordList.length)];
-      var el = document.getElementById('word');
-      el.innerText = word;
-      say(word);
-    });
+  fetch('https://raw.githubusercontent.com/alexanderwassberg/ordflaket2.0/main/words.txt')
+  .then(response => response.text())
+  .then(data => {
+    var wordList = data.split("\n");
+    var word = wordList[Math.floor(Math.random() * wordList.length)];
+    var el = document.getElementById('word');
+    el.innerText = word;
+    say(word);
+  });
 }
 
 function say(m) {
-    var msg = new SpeechSynthesisUtterance();
-    var voices = window.speechSynthesis.getVoices();
-    msg.voice = voices[9];
-    msg.voiceURI = "native";
-    msg.volume = 1;
-    msg.rate = 1.2;
-    msg.pitch = 1;
-    msg.text = m;
-    msg.lang = 'sv-SE';
-    speechSynthesis.speak(msg);
+  var msg = new SpeechSynthesisUtterance();
+  var voices = window.speechSynthesis.getVoices();
+  msg.voice = voices[9];
+  msg.voiceURI = "native";
+  msg.volume = 1;
+  msg.rate = 1.2;
+  msg.pitch = 1;
+  msg.text = m;
+  msg.lang = 'sv-SE';
+  speechSynthesis.speak(msg);
 }
