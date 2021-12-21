1cf1b64d83e0fe62ffa094931f4c79b4b67204fe
alexanderwassberg <alexanderwassberg@gmail.com>
2021-12-21 20:39:34 +0100

    CopyToClipboard

--- script/wordGen.js
+++ script/wordGen.js
@@ -43,3 +43,28 @@ function say(m) {
   msg.lang = 'sv-SE';
   speechSynthesis.speak(msg);
 }
+
+function copyWord() {
+  var textToCopy = document.querySelector('h1').innerText;
+
+  if (navigator.clipboard && window.isSecureContext) {
+      // navigator clipboard api method'
+      return navigator.clipboard.writeText(textToCopy);
+  } else {
+      // text area method
+      let textArea = document.createElement("textarea");
+      textArea.value = textToCopy;
+      // make the textarea out of viewport
+      textArea.style.position = "fixed";
+      textArea.style.left = "-999999px";
+      textArea.style.top = "-999999px";
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+      return new Promise((res, rej) => {
+          // here the magic happens
+          document.execCommand('copy') ? res() : rej();
+          textArea.remove();
+      });
+  }
+}
