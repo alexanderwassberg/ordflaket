367fa7c3d21811520db9c4b98a8d6258836f3dd2
alexander.wassberg <alexanderwassberg@gmail.com>
2021-12-21 17:12:16 +0100

    Random combinations

--- script/wordGen.js
+++ script/wordGen.js
@@ -1,17 +1,36 @@
+// Generates the first word when visiting ordlflaket
 getWord();
 
+
 function getWord() {
+  // Fetching repository textfile
   fetch('https://raw.githubusercontent.com/alexanderwassberg/ordflaket/master/words.txt')
   .then(response => response.text())
   .then(data => {
     var wordList = data.split("\n");
-    var word = wordList[Math.floor(Math.random() * wordList.length)];
+    var wordsLeft = [];
+    var wordsRight = [];
+
+    for (var i = 0; i < wordList.length; i++) {
+      var words = wordList[i].split("-");
+      wordsLeft[i]   = words[0];
+      wordsRight[i]  = words[1];
+      var word = wordsLeft[getRandomInt(wordList.length)]+"-"+wordsRight[getRandomInt(wordList.length)];
+    }
+
     var el = document.getElementById('word');
     el.innerText = word;
     say(word);
+
+
   });
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Adds the voices that reads the generated word
 function say(m) {
   var msg = new SpeechSynthesisUtterance();
   var voices = window.speechSynthesis.getVoices();
