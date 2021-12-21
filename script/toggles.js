232ec5e5d827ef3cfba05b76db755ffb1f95ac13
alexanderwassberg <alexanderwassberg@gmail.com>
2021-12-21 23:28:51 +0100

    Voice Settings

---
+++ script/toggles.js
@@ -0,0 +1,12 @@
+const btnVoice = document.querySelector('.btn-voice');
+const btnVoiceClose = document.querySelector('.btn-voice-close');
+const voiceChanger = document.querySelector('.voice-changer');
+
+function toggleVoices() {
+    voiceChanger.classList.toggle('active');
+    btnVoice.innerHTML = "<i class='fa fa-times-circle'></i>";
+
+    if(!voiceChanger.classList.contains('active')){
+        btnVoice.innerHTML = "<i class='fa fa-cog'></i>";
+    }
+}
