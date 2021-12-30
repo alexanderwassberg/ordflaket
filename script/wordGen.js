8cae8103d2724c862809f0a9e916f203afab8914
alexanderwassberg <alexanderwassberg@gmail.com>
2021-12-30 11:21:05 +0100

    no comments

--- script/wordGen.js
+++ script/wordGen.js
@@ -1,39 +1,33 @@
 function getWord() {
 
     var voice_id = document.getElementById("voice").selectedIndex;
-
-    // Fetching Raw Github Repo text file for faster maintaining
+    
     fetch('https://raw.githubusercontent.com/alexanderwassberg/ordflaket/master/words.txt')
-    .then(response => response.text())
-    .then(data => {
-        // Split (on row) text file into a array
-        var wordList = data.split("\n");
+        .then(response => response.text())
+        .then(data => {
 
-        // Creating two new arrays for each side of the "-" mark
-        var wordsLeft = [];
-        var wordsRight = [];
+            function getRandomInt(max) {
+                return Math.floor(Math.random() * max);
+            }
 
-        for (var i = 0; i < wordList.length; i++) {
-            var words = wordList[i].split("-");
-            wordsLeft[i] = words[0];
-            wordsRight[i] = words[1];
-            var word = wordsLeft[getRandomInt(wordList.length)] + '-' + wordsRight[getRandomInt(wordList.length)];
-        }
+            var wordList = data.split("\n");
+            var wordsLeft = [];
+            var wordsRight = [];
 
-        // Targets the H1-element for pushing the word
-        var el = document.getElementById('word');
-        el.innerText = word;
-        say(word);
+            for (var i = 0; i < wordList.length; i++) {
+                var words = wordList[i].split("-");
+                wordsLeft[i] = words[0];
+                wordsRight[i] = words[1];
+                var word = wordsLeft[getRandomInt(wordList.length)] + '-' + wordsRight[getRandomInt(wordList.length)];
+            }
 
-    });
+            var el = document.getElementById('word');
+            el.innerText = word;
+            say(word);
+
+        });
 }
 
-// Randomize function
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-// Let's the user change the voice
 function populate_voices() {
     var voices = window.speechSynthesis.getVoices();
     var option_index = 0;
@@ -49,7 +43,6 @@ function populate_voices() {
     });
 }
 
-// Voice Over functionality
 function say(m) {
     var voice_id = document.getElementById("voice").selectedIndex;
     var volumeSlide = document.getElementById("volume").value;
@@ -68,26 +61,23 @@ function say(m) {
     speechSynthesis.speak(msg);
 }
 
-// Copies the generated word the the Clipboard by clicking the text of the word
 function copyWord() {
     var textToCopy = document.querySelector('h1').innerText;
 
     if (navigator.clipboard && window.isSecureContext) {
-        // navigator clipboard api method'
         return navigator.clipboard.writeText(textToCopy);
     } else {
-        // text area method
         let textArea = document.createElement("textarea");
         textArea.value = textToCopy;
-        // make the textarea out of viewport
+
         textArea.style.position = "fixed";
         textArea.style.left = "-999999px";
         textArea.style.top = "-999999px";
         document.body.appendChild(textArea);
         textArea.focus();
         textArea.select();
+
         return new Promise((res, rej) => {
-            // here the magic happens
             document.execCommand('copy') ? res() : rej();
             textArea.remove();
         });
